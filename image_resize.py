2de70cf0eac433a9fbdf4fca7201ab7a75f66f2d
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-01 16:53:29 +0500

    resize image func

--- image_resize.py
+++ image_resize.py
@@ -7,9 +7,28 @@ def load_image():
     return image
 
 
-def resize_image(path_to_original, path_to_result):
+def resize_image(image, width=None, height=None, scale=None, path_to_result='./__pic.jpg'):
+    old_image_width = image.size[0]
+    old_image_height = image.size[1]
+    if width == None and height != None:
+        size_difference = abs(height - old_image_height)
+        width = old_image_width - size_difference
+    if height == None and width != None:
+        size_difference = abs(width - old_image_width)
+        height = old_image_height - size_difference
+    if scale != None:
+        resized_image = image.resize((width * scale, height * scale))
+    else:
+        resized_image = image.resize((width, height))
+    return resized_image
+
+
+def save_image(image, filepath='./'):
     pass
 
 
+
 if __name__ == '__main__':
-    load_image()
+    image = load_image()
+    resized_image = resize_image(image, width=300, height=300)
+    save_image(resized_image)
